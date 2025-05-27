7f6c744767032e1e9a141cfabc95c4321733f34d
Minji <MJnotZ0@gmail.com>
2025-05-27 18:23:50 +0900

    리액트로 변경

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import GalleryPage from './pages/gallery/GalleryPage';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <GalleryPage />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from '../css/Header.css';
+
+const Header = () => {
+    return (
+        <div className="main-header">
+            <h1>Nocturnal</h1>
+        </div>
+    );
+};
+
+export default Header;
